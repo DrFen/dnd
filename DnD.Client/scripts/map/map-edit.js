55c955e2d02bd61782e52d42c16d750dfa6b73e2
Makarov Sergey <makarov-sl@dartit.ru makarov-sl@dartit.ru>
2016-02-15 18:07:34 +0500

    map editing

--- DnD.Client/scripts/map/map-edit.js
+++ DnD.Client/scripts/map/map-edit.js
@@ -35,22 +35,41 @@
 				}
 				return false;
 			},
-			getActiveIndex : function(){
+			getActiveLayerIndex : function(){
 				for(var i = 0; i < this.Layers.length; i++){
 					if(this.Layers[i].Id === this.ActiveLayer)
 						return i;
 				}
+			},
+			fillSelectedMapItem : function(){		
+				if(!this.SelectedMapItem){
+					this.SelectedMapItem = {Id: undefined};
+					return;
+				}
+				for(var i = 0; i < $scope.presetList.length; i++){
+					if($scope.presetList[i].Id === this.SelectedMapItem.Id){
+						for(property in $scope.presetList[i]){
+							if(property === 'Id')
+								continue;
+							//if(SelectedMapItem.hasOwnProperty(property){
+								this.SelectedMapItem[property] = $scope.presetList[i][property];
+							//}								
+						}						
+						return;
+					}
+				};
+				this.SelectedMapItem = {Id: undefined};
 			}
 			
 		}
 		
 		
 		$scope.presetList = [
-			{Id: 'p1', PreviewImg: 'sprites/item.jpg', Image: 'sprites/item.jpg', Name: 'preset 1', Width: 1, Height: 1, Parameters:[]},
-			{Id: 'p2', PreviewImg: 'sprites/exampe_terraine.jpg', Image: 'sprites/exampe_terraine.jpg'  , Name: 'preset 1', Width: 1, Height: 1, Parameters:[]},
-			{Id: 'p3', PreviewImg: 'sprites/exampe_terraine2.jpg', Image: 'sprites/exampe_terraine2.jpg', Name: 'preset 2:1', Width: 2, Height: 1, Parameters:[]},
-			{Id: 'p4', PreviewImg: 'sprites/exampe_terraine3.jpg', Image: 'sprites/exampe_terraine3.jpg', Name: 'preset 1:2', Width: 1, Height: 2, Parameters:[]},
-			{Id: 'p5', PreviewImg: 'sprites/exampe_terraine4.jpg', Image: 'sprites/exampe_terraine4.jpg', Name: 'preset 2:2', Width: 2, Height: 2, Parameters:[]}
+			{Id: 'p1', PreviewImg: 'sprites/item.jpg',             Image: 'sprites/item.jpg',             Name: 'preset 1',   Width: 1, Height: 1, Rotation: 0, Parameters:[]},
+			{Id: 'p2', PreviewImg: 'sprites/exampe_terraine.jpg',  Image: 'sprites/exampe_terraine.jpg' , Name: 'preset 1',   Width: 1, Height: 1, Rotation: 0, Parameters:[]},
+			{Id: 'p3', PreviewImg: 'sprites/exampe_terraine2.jpg', Image: 'sprites/exampe_terraine2.jpg', Name: 'preset 2:1', Width: 2, Height: 1, Rotation: 0, Parameters:[]},
+			{Id: 'p4', PreviewImg: 'sprites/exampe_terraine3.jpg', Image: 'sprites/exampe_terraine3.jpg', Name: 'preset 1:2', Width: 1, Height: 2, Rotation: 0, Parameters:[]},
+			{Id: 'p5', PreviewImg: 'sprites/exampe_terraine4.jpg', Image: 'sprites/exampe_terraine4.jpg', Name: 'preset 2:2', Width: 2, Height: 2, Rotation: 0, Parameters:[]}
 		];
 
 			
@@ -113,7 +132,7 @@
 	$scope.formatPreset = function(data){
 		for(var i=0; i<$scope.presetList.length; i++){
 			if($scope.presetList[i].Id === data.id)
-				return '<img style="width:20px; height:20px;" src="' + '../../'+$scope.presetList[i].PreviewImg+ '"/>&nbsp;&nbsp;' + data.text;
+				return '<img style="width:15px; height:15px;" src="' + '../../'+$scope.presetList[i].PreviewImg+ '"/>&nbsp;&nbsp;' + data.text;
 				
 		}
 		
@@ -148,7 +167,7 @@
 		}
 		
 		$scope.squareArray[layerId + 'Layer'] = [];
-		$scope.envVariable.Layers.splice($scope.envVariable.getActiveIndex() + 1, 0, {Id : layerId, Name : layerName, Show: true});
+		$scope.envVariable.Layers.splice($scope.envVariable.getActiveLayerIndex() + 1, 0, {Id : layerId, Name : layerName, Show: true});
 
 
 	}
@@ -161,7 +180,13 @@
 				if($scope.envVariable.Layers[i].Show && $scope.envVariable.Layers[i].Id !== 'Background')	
 					$scope.squareArray.Layers.push($scope.envVariable.Layers[i].Id);
 			};
-		}, true);
+	}, true);
+		
+	$scope.$watch('envVariable.SelectedMapItem.Id', function(){
+		//debugger;
+		$scope.envVariable.fillSelectedMapItem();
+		console.log($scope.envVariable.SelectedMapItem);
+	}, true);	
 		
 	$scope.onChangeVisibility = function(oldValue, layerId){
 		if(oldValue && $scope.envVariable.ActiveLayer === layerId)
